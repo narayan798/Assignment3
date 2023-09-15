0a4705844747f73fbe7ea79e9affc349b6455138
narayan798 <143153731+narayan798@users.noreply.github.com>
2023-09-15 09:14:38 +0530

    Add files via upload

---
+++ ques 10/ques 10/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace que10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    public class Class1
+    {
+        public Class1()
+        {
+            Console.WriteLine("Instance Created !");
+        }
+    }
+}
